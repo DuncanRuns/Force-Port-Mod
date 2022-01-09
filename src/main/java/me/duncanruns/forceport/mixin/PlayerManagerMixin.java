f914fde8d53d8b874ccab2a8dadfaca9fa4948f0
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2022-01-09 12:45:50 -0400

    I forgot to commit this stuff before release oh well

--- src/main/java/me/duncanruns/forceport/mixin/PlayerManagerMixin.java
+++ src/main/java/me/duncanruns/forceport/mixin/PlayerManagerMixin.java
@@ -1,12 +1,9 @@
-
-
-
 package me.duncanruns.forceport.mixin;
 
 import net.minecraft.server.PlayerManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
@@ -14,8 +11,8 @@ public abstract class PlayerManagerMixin {
      * @author AppDevMichael
      * @reason Replace the default 8 player limit with 20
      */
-    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"))
-    public static int changeMaxPlayers(int maxPlayers) {
+    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"), argsOnly = true)
+    private static int changeMaxPlayers(int maxPlayers) {
         if (maxPlayers == 8) { return 20; }
         return maxPlayers;
     }
